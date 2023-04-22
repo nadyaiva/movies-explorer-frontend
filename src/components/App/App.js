70f71fe49f6e976f263053b0b19334b75a8ad164
nadyaiva <79001245+nadyaiva@users.noreply.github.com>
2023-04-22 17:28:26 +0200

    Setup base stracture

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Route, Switch, useHistory } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bioscoop</h1>
+    </div>
+  );
+}
+
+export default App;

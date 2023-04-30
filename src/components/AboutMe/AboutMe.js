59d7bf5f67d24a5573a66fa7ee591f64091c488f
nadyaiva <79001245+nadyaiva@users.noreply.github.com>
2023-04-30 19:07:53 +0200

    Style about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,16 +6,18 @@ import Portfolio from "../Portfolio/Portfolio";
 function AboutMe() {
     return (
         <section className="section-container">
-        <div className="about-me">
             <SectionTitle title="Обо мне" />
+        <div className="about-me">
             <img className="about-me__photo" src={photo} />
-            <h3 className="about-me__name">Nadya</h3>
-            <p className="about-me__job">Фронтенд-разработчик, 30 лет</p>
-            <p className="about-me__description">Я родился и живу в Саратове, 
-            закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, 
-            а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». 
-            После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-            <a className="about-me__website"></a>
+            <div className="about-me__info">
+                <h3 className="about-me__name">Надя</h3>
+                <p className="about-me__job">Фронтенд-разработчик, 30 лет</p>
+                <p className="about-me__description">Я родился и живу в Саратове, 
+                закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, 
+                а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». 
+                После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+                <a href="https://github.com/nadyaiva" className="about-me__website" target="_blank"></a>
+            </div>
         </div>
             <Portfolio />
         </section>
